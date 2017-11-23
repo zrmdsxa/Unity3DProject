2efdae79f6858160275c3e11aececaaa935579cb
Tin Lun Mui <undefinedhoujuunue@gmail.com>
2017-11-23 00:32:12 -0500

    create project + toon soldier model w/ anims

---
+++ Assets/Scenes/Destroy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+	public float time = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		Destroy(gameObject,time);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
